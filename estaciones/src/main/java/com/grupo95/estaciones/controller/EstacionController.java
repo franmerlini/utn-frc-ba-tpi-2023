2fdae5e37748768de9cb99e73dc6d74077505ed8
Fabrizio Chavez <83493@sistemas.frc.utn.edu.ar>
2023-10-30 22:22:20 -0300

    feat: Add functionality 1. Get all stations

--- estaciones/src/main/java/com/grupo95/estaciones/controller/EstacionController.java
+++ estaciones/src/main/java/com/grupo95/estaciones/controller/EstacionController.java
@@ -4,6 +4,7 @@ import com.grupo95.estaciones.entity.EstacionEntity;
 import com.grupo95.estaciones.service.EstacionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,18 +13,23 @@ import java.util.List;
 @RequestMapping("/api/estaciones")
 @RequiredArgsConstructor
 public class EstacionController {
-    private final EstacionService estacionService;
+    private final EstacionService _estacionService;
 
     @GetMapping()
-    @ResponseStatus(HttpStatus.OK)
-    public List<EstacionEntity> obtenerEstaciones() {
-        return estacionService.obtenerEstaciones();
+    public ResponseEntity<List<EstacionEntity>> obtenerEstaciones() {
+        List<EstacionEntity> response;
+        try{
+            response = _estacionService.obtenerEstaciones();
+        }catch (Exception ex){
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok(response);
     }
 
     // TODO: Add guard conditions
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public void agregarEstacion(@RequestBody EstacionEntity estacion) {
-        estacionService.agregarEstacion(estacion);
+        _estacionService.agregarEstacion(estacion);
     }
 }
