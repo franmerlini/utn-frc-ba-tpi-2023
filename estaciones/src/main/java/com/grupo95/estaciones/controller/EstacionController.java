d0ce666ca83787d5e41e2a7c6ba15a4ac9caee0a
Fabrizio Chavez <83493@sistemas.frc.utn.edu.ar>
2023-10-30 23:20:06 -0300

    feat: Add functionality number 2 "GetStationByCoordinates"

--- estaciones/src/main/java/com/grupo95/estaciones/controller/EstacionController.java
+++ estaciones/src/main/java/com/grupo95/estaciones/controller/EstacionController.java
@@ -1,13 +1,17 @@
 package com.grupo95.estaciones.controller;
 
+import com.grupo95.estaciones.EstacionesApplication;
 import com.grupo95.estaciones.entity.EstacionEntity;
 import com.grupo95.estaciones.service.EstacionService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/estaciones")
@@ -27,6 +31,25 @@ public class EstacionController {
     }
 
     // TODO: Add guard conditions
+
+    @Operation(
+            summary = "Get Nearest Station by Coordinates",
+            description = "Retrieve the closest station based on provided latitude and longitude."
+    )
+    @GetMapping("/{lat}&{lng}")
+    public ResponseEntity<EstacionEntity> GetStationByCoordinates(@PathVariable double lat, @PathVariable double lng){
+        EstacionEntity response;
+        try {
+            response = _estacionService.getStationByCoordinates(lat, lng);
+        } catch (NoSuchElementException ex) {
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().build();
+        } catch (Exception ex) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok(response);
+    }
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public void agregarEstacion(@RequestBody EstacionEntity estacion) {
